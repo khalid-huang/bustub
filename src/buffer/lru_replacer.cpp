a8be411c0695223d59c356a2a9e58e76fc87272b
khalid-huang <khalid_huang@163.com>
2022-08-24 21:07:29 +0800

    finish project01 task #1

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -14,16 +14,57 @@
 
 namespace bustub {
 
-LRUReplacer::LRUReplacer(size_t num_pages) {}
+LRUReplacer::LRUReplacer(size_t num_pages) {
+    capacity = num_pages;
+}
 
 LRUReplacer::~LRUReplacer() = default;
 
-auto LRUReplacer::Victim(frame_id_t *frame_id) -> bool { return false; }
+auto LRUReplacer::Victim(frame_id_t *frame_id) -> bool {
+    latch.lock();
+    // 判断是否为空，为空返回false
+    if (data.empty()) {
+        latch.unlock();
+        return false;
+    }
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+    // 不为空移除链表最后一个，同时删除map里面的
+    frame_id_t victim = lruList.back();
+    data.erase(victim);
+    lruList.pop_back();
+    *frame_id = victim;
+    latch.unlock();
+    return true;
+}
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+void LRUReplacer::Pin(frame_id_t frame_id) {
+    latch.lock();
+    // 找到就删除
+    if (data.count(frame_id) != 0) {
+        lruList.erase(data[frame_id]);
+        data.erase(frame_id);
+    } 
+    latch.unlock();
+}
 
-auto LRUReplacer::Size() -> size_t { return 0; }
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+    latch.lock();
+    // 确定不在才操作
+    if (data.count(frame_id) == 0) {
+        // 不断异常链表末尾的，直到容量足够
+        while (static_cast<int>(Size()) >= capacity) {
+            data.erase(lruList.back());
+            lruList.pop_back();
+        }
+
+        lruList.push_front(frame_id);
+        data[frame_id] = lruList.begin();
+    }
+    latch.unlock();
+}
+
+auto LRUReplacer::Size() -> size_t {
+    return data.size();
+}
 
 }  // namespace bustub
