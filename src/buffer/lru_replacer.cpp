d457b9d94fbebd7a1064bc6dcbeaab402f81bb02
khalid-huang <khalid_huang@163.com>
2022-08-24 21:52:32 +0800

    finish project01 task#1

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -14,16 +14,61 @@
 
 namespace bustub {
 
-LRUReplacer::LRUReplacer(size_t num_pages) {}
+LRUReplacer::LRUReplacer(size_t num_pages) {
+    capacity = num_pages;
+}
 
 LRUReplacer::~LRUReplacer() = default;
 
-bool LRUReplacer::Victim(frame_id_t *frame_id) { return false; }
+bool LRUReplacer::Victim(frame_id_t *frame_id) { 
+    latch.lock();
+    // 如果为空就返回false
+    if (Size() == 0) {
+        latch.unlock();
+        return false;
+    }
+    // 从尾部去掉一个
+    frame_id_t victim = lruList.back();
+    data.erase(victim);
+    lruList.pop_back();
+    *frame_id = victim;
+    latch.unlock();
+    return true;
+}
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+void LRUReplacer::Pin(frame_id_t frame_id) {
+    latch.lock();
+    // 如果存在lru里面，就去除掉，避免改页被淘汰了
+    if (data.count(frame_id) == 0) {
+        latch.unlock();
+        return;
+    }
+    lruList.erase(data[frame_id]);
+    data.erase(frame_id);
+    latch.unlock();
+}
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+    latch.lock(); 
+    // 如果已经存在，就跳过
+    if (data.count(frame_id) != 0) {
+        latch.unlock();
+        return;
+    }
+    // 确保有空间
+    while (Size() >= capacity) {
+        // 不断从lru尾部移除
+        frame_id_t victim = lruList.back();
+        data.erase(victim);
+        lruList.pop_back();
+    }
+    lruList.push_front(frame_id);
+    data[frame_id] = lruList.begin();
+    latch.unlock();
+}
 
-size_t LRUReplacer::Size() { return 0; }
+size_t LRUReplacer::Size() {
+    return data.size();
+}
 
 }  // namespace bustub
